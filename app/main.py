11f93b8ea7799e71859fa130e20a3203f178d697
zolppy <gabriel.lcifba@gmail.com>
2025-10-05 16:33:37 -0300

    feat: index route

--- app/main.py
+++ app/main.py
@@ -7,3 +7,8 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(router=api_v1_router, prefix="/api/v1")
+
+
+@app.get("/")
+def index():
+    return "Documentation: http://127.0.0.1:8000/docs"
